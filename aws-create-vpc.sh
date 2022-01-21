218ec7399ba73c84cbedbeb604b18b5203eef485
mittanmayank <97217764+mittanmayank@users.noreply.github.com>
2022-01-21 10:09:15 +0530

    Update aws-create-vpc.sh

--- aws-create-vpc.sh
+++ aws-create-vpc.sh
@@ -1,41 +1,14 @@
 #!/bin/bash
-#******************************************************************************
-#    AWS VPC Creation Shell Script
-#******************************************************************************
-#
-# SYNOPSIS
-#    Automates the creation of a custom IPv4 VPC, having both a public and a
-#    private subnet, and a NAT gateway.
-#
-# DESCRIPTION
-#    This shell script leverages the AWS Command Line Interface (AWS CLI) to
-#    automatically create a custom VPC.  The script assumes the AWS CLI is
-#    installed and configured with the necessary security credentials.
-#
-#==============================================================================
-#
-# NOTES
-#   VERSION:   0.1.0
-#   LASTEDIT:  03/18/2017
-#   AUTHOR:    Joe Arauzo
-#   EMAIL:     joe@arauzo.net
-#   REVISIONS:
-#       0.1.0  03/18/2017 - first release
-#       0.0.1  02/25/2017 - work in progress
-#
-#==============================================================================
-#   MODIFY THE SETTINGS BELOW
-#==============================================================================
-#
-AWS_REGION="us-west-1"
-VPC_NAME="My VPC"
+
+AWS_REGION="ap-south-1"
+VPC_NAME="demoVPC"
 VPC_CIDR="10.0.0.0/16"
 SUBNET_PUBLIC_CIDR="10.0.1.0/24"
-SUBNET_PUBLIC_AZ="us-west-1a"
-SUBNET_PUBLIC_NAME="10.0.1.0 - us-west-1a"
+SUBNET_PUBLIC_AZ="ap-south-1"
+SUBNET_PUBLIC_NAME="10.0.1.0 - ap-south-1"
 SUBNET_PRIVATE_CIDR="10.0.2.0/24"
-SUBNET_PRIVATE_AZ="us-west-1c"
-SUBNET_PRIVATE_NAME="10.0.2.0 - us-west-1b"
+SUBNET_PRIVATE_AZ="ap-south-1"
+SUBNET_PRIVATE_NAME="10.0.2.0 - ap-south-1"
 CHECK_FREQUENCY=5
 #
 #==============================================================================
@@ -147,61 +120,64 @@ echo "  'Auto-assign Public IP' ENABLED on Public Subnet ID" \
   "'$SUBNET_PUBLIC_ID'."
 
 # Allocate Elastic IP Address for NAT Gateway
-echo "Creating NAT Gateway..."
-EIP_ALLOC_ID=$(aws ec2 allocate-address \
-  --domain vpc \
-  --query '{AllocationId:AllocationId}' \
-  --output text \
-  --region $AWS_REGION)
-echo "  Elastic IP address ID '$EIP_ALLOC_ID' ALLOCATED."
+#echo "Creating NAT Gateway..."
+#EIP_ALLOC_ID=$(aws ec2 allocate-address \
+#  --domain vpc \
+#  --query '{AllocationId:AllocationId}' \
+#  --output text \
+#  --region $AWS_REGION)
+#echo "  Elastic IP address ID '$EIP_ALLOC_ID' ALLOCATED."
 
 # Create NAT Gateway
-NAT_GW_ID=$(aws ec2 create-nat-gateway \
-  --subnet-id $SUBNET_PUBLIC_ID \
-  --allocation-id $EIP_ALLOC_ID \
-  --query 'NatGateway.{NatGatewayId:NatGatewayId}' \
-  --output text \
-  --region $AWS_REGION)
-FORMATTED_MSG="Creating NAT Gateway ID '$NAT_GW_ID' and waiting for it to "
-FORMATTED_MSG+="become available.\n    Please BE PATIENT as this can take some "
-FORMATTED_MSG+="time to complete.\n    ......\n"
-printf "  $FORMATTED_MSG"
-FORMATTED_MSG="STATUS: %s  -  %02dh:%02dm:%02ds elapsed while waiting for NAT "
-FORMATTED_MSG+="Gateway to become available..."
-SECONDS=0
-LAST_CHECK=0
-STATE='PENDING'
-until [[ $STATE == 'AVAILABLE' ]]; do
-  INTERVAL=$SECONDS-$LAST_CHECK
-  if [[ $INTERVAL -ge $CHECK_FREQUENCY ]]; then
-    STATE=$(aws ec2 describe-nat-gateways \
-      --nat-gateway-ids $NAT_GW_ID \
-      --query 'NatGateways[*].{State:State}' \
-      --output text \
-      --region $AWS_REGION)
-    STATE=$(echo $STATE | tr '[:lower:]' '[:upper:]')
-    LAST_CHECK=$SECONDS
-  fi
-  SECS=$SECONDS
-  STATUS_MSG=$(printf "$FORMATTED_MSG" \
-    $STATE $(($SECS/3600)) $(($SECS%3600/60)) $(($SECS%60)))
-  printf "    $STATUS_MSG\033[0K\r"
-  sleep 1
-done
-printf "\n    ......\n  NAT Gateway ID '$NAT_GW_ID' is now AVAILABLE.\n"
+
+#3NAT_GW_ID=$(aws ec2 create-nat-gateway \
+#  --subnet-id $SUBNET_PUBLIC_ID \
+#  --allocation-id $EIP_ALLOC_ID \
+#  --query 'NatGateway.{NatGatewayId:NatGatewayId}' \
+#  --output text \
+#  --region $AWS_REGION)
+#FORMATTED_MSG="Creating NAT Gateway ID '$NAT_GW_ID' and waiting for it to "
+#FORMATTED_MSG+="become available.\n    Please BE PATIENT as this can take some "
+#FORMATTED_MSG+="time to complete.\n    ......\n"
+#printf "  $FORMATTED_MSG"
+#FORMATTED_MSG="STATUS: %s  -  %02dh:%02dm:%02ds elapsed while waiting for NAT "
+#FORMATTED_MSG+="Gateway to become available..."
+#SECONDS=0
+#LAST_CHECK=0
+#STATE='PENDING'
+#until [[ $STATE == 'AVAILABLE' ]]; do
+#  INTERVAL=$SECONDS-$LAST_CHECK
+#  if [[ $INTERVAL -ge $CHECK_FREQUENCY ]]; then
+#
+#STATE=$(aws ec2 describe-nat-gateways \
+#     --nat-gateway-ids $NAT_GW_ID \
+#      --query 'NatGateways[*].{State:State}' \
+#      --output text \
+#      --region $AWS_REGION)
+#    STATE=$(echo $STATE | tr '[:lower:]' '[:upper:]')
+#    LAST_CHECK=$SECONDS
+#  fi
+#  SECS=$SECONDS
+#  STATUS_MSG=$(printf "$FORMATTED_MSG" \
+#    $STATE $(($SECS/3600)) $(($SECS%3600/60)) $(($SECS%60)))
+#  printf "    $STATUS_MSG\033[0K\r"
+#  sleep 1
+#done
+#printf "\n    ......\n  NAT Gateway ID '$NAT_GW_ID' is now AVAILABLE.\n"
 
 # Create route to NAT Gateway
-MAIN_ROUTE_TABLE_ID=$(aws ec2 describe-route-tables \
-  --filters Name=vpc-id,Values=$VPC_ID Name=association.main,Values=true \
-  --query 'RouteTables[*].{RouteTableId:RouteTableId}' \
-  --output text \
-  --region $AWS_REGION)
-echo "  Main Route Table ID is '$MAIN_ROUTE_TABLE_ID'."
-RESULT=$(aws ec2 create-route \
-  --route-table-id $MAIN_ROUTE_TABLE_ID \
-  --destination-cidr-block 0.0.0.0/0 \
-  --gateway-id $NAT_GW_ID \
-  --region $AWS_REGION)
-echo "  Route to '0.0.0.0/0' via NAT Gateway with ID '$NAT_GW_ID' ADDED to" \
-  "Route Table ID '$MAIN_ROUTE_TABLE_ID'."
-echo "COMPLETED"
+
+#MAIN_ROUTE_TABLE_ID=$(aws ec2 describe-route-tables \
+#  --filters Name=vpc-id,Values=$VPC_ID Name=association.main,Values=true \
+#  --query 'RouteTables[*].{RouteTableId:RouteTableId}' \
+#  --output text \
+#  --region $AWS_REGION)
+#echo "  Main Route Table ID is '$MAIN_ROUTE_TABLE_ID'."
+#RESULT=$(aws ec2 create-route \
+#  --route-table-id $MAIN_ROUTE_TABLE_ID \
+#  --destination-cidr-block 0.0.0.0/0 \
+#  --gateway-id $NAT_GW_ID \
+#  --region $AWS_REGION)
+#echo "  Route to '0.0.0.0/0' via NAT Gateway with ID '$NAT_GW_ID' ADDED to" \
+#  "Route Table ID '$MAIN_ROUTE_TABLE_ID'."
+#echo "COMPLETED"
